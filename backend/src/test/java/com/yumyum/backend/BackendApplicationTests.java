ff00f16d35221dcbb48ae39b139ed823833bdda7
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-06-25 23:12:12 +0530

    Backend Project setup done

---
+++ backend/src/test/java/com/yumyum/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumyum.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
